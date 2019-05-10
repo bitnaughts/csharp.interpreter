d50aea755baf5b26fdbaffc9ed9a6e97d611cde1
Brian <brianhungerman@gmail.com>
2019-05-09 22:23:18 -0700

    Cleaning PEDMAS template

--- Constants/Operators.cs
+++ Constants/Operators.cs
@@ -28,17 +28,17 @@
         CLOSING_PARENTHESIS = ")",
         END_LINE = ";";
 
-        //public const string[] ALL_OPERATORS = { EQUALS, INCREMENT, DECREMENT, ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION, MODULO, EQUAL_TO, NOT_EQUAL, GREATER_THAN, GREATER_THAN_EQUAL, LESS_THAN, LESS_THAN_EQUAL, AND, OR, MODULUS, TIMES, DIVIDE, ADD, SUBTRACT };
+    //public const string[] ALL_OPERATORS = { EQUALS, INCREMENT, DECREMENT, ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION, MODULO, EQUAL_TO, NOT_EQUAL, GREATER_THAN, GREATER_THAN_EQUAL, LESS_THAN, LESS_THAN_EQUAL, AND, OR, MODULUS, TIMES, DIVIDE, ADD, SUBTRACT };
 
     public const char END_LINE_CHAR = ';';
 
     public static readonly string[][] PEMDAS = {
-        new string[] { "%" },
-        new string[] { "*", "/" },
-        new string[] { "+", "-" },
-        new string[] { "==", "!=", ">", ">=", "<", "<=" },
-        new string[] { "&&" },
-        new string[] { "||" }
+        new string[] { MODULUS },
+        new string[] { TIMES, DIVIDE },
+        new string[] { ADD, SUBTRACT },
+        new string[] { EQUAL_TO, NOT_EQUAL, GREATER_THAN, GREATER_THAN_EQUAL, LESS_THAN, LESS_THAN_EQUAL },
+        new string[] { AND },
+        new string[] { OR }
     };
 
 }
