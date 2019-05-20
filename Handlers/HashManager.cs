8105dd9fb00477587715020b6faaec53093d051a
Brian <brianhungerman@gmail.com>
2019-05-20 15:37:34 -0700

    Create HashManager.cs

---
+++ Handlers/HashManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class HashManager {
+    //The purpose of this class is to assign unique hashes to objects to correlate them between seperate scripts
+    static int hash_counter = 0;
+
+    public static int getNewHash() {
+        return hash_counter++;
+    }
+    public static void retireHash() {
+
+    }
+}
