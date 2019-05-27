32b43e0ae2e26c9eff38c6ea1d04bbbf39baaedb
Brian <brianhungerman@gmail.com>
2019-05-26 19:32:55 -0700

    Add template dictionary

--- Handlers/TemplateHandler.cs
+++ Handlers/TemplateHandler.cs
@@ -1,8 +1,19 @@
 using System;
 using System.Collections.Generic;
 
-public static class TemplateHandler {
+public class TemplateHandler {
 
+    public Dictionary<string, string[,]> template = new Dictionary<string, string[,]> ();
 
+    public TemplateHandler() {
+        
+    }
+
+    public void addTemplate(string key, string[,] template_array) {
+
+    }
+    public string[,] getTemplate(string key) {
+
+    }
     
 }
