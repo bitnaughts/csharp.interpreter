f587a4238c1958f07b717da14834a55b506a3566
Brian <brianhungerman@gmail.com>
2019-05-26 19:32:38 -0700

    Create Templates.cs

---
+++ Constants/Templates.cs
@@ -0,0 +1,11 @@
+﻿public static class Templates {
+    public const string[, ]  IF = [[Operators.OPENING_PARENTHESIS, Operators.CLOSING_PARENTHESIS]],
+        WHILE = "while",
+        FOR = "for",
+        STATIC = "static",
+        NEW = "new",
+        NULL = "null",
+        LIBRARY_IMPORT = "using";
+
+    // public const string[] ALL_KEYWORDS = { BREAK, CONTINUE, IF, WHILE, FOR, LIBRARY_IMPORT };
+}
