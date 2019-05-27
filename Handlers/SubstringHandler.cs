537ae28270ead0b9bef6e80730f26db90ff1df21
Brian <brianhungerman@gmail.com>
2019-05-27 14:44:13 -0700

    Splitting line parameters (with stringer delimiters)

--- Handlers/SubstringHandler.cs
+++ Handlers/SubstringHandler.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 public static class SubstringHandler {
 
-    enum SCAN_OPTION { FIRST_OCCURANCE, LAST_OCCURANCE, FIRST_OCCURANCE_IN_SAME_SCOPE };
+    enum SCAN_OPTION { FIRST_OCCURANCE, LAST_OCCURANCE, NEXT_OCCURANCE_IN_SCOPE };
 
     /*
         String Class Overloads:
@@ -23,7 +23,7 @@ public static class SubstringHandler {
             case SCAN_OPTION.FIRST_OCCURANCE:
                 line = line.Substring(line.IndexOf(start));
                 break;
-            case SCAN_OPTION.FIRST_OCCURANCE_IN_SAME_SCOPE:
+            case SCAN_OPTION.NEXT_OCCURANCE_IN_SCOPE:
 
                 break;
             case start_scan_option.LAST_OCCURANCE:
@@ -34,8 +34,8 @@ public static class SubstringHandler {
             case SCAN_OPTION.FIRST_OCCURANCE:
                 line = line.Substring(0, line.IndexOf(end));
                 break;
-            case SCAN_OPTION.FIRST_OCCURANCE_IN_SAME_SCOPE:
-
+            case SCAN_OPTION.NEXT_OCCURANCE_IN_SCOPE:
+                line = line.Substring(0, IndexInScopeOf(line, end));
                 break;
             case start_scan_option.LAST_OCCURANCE:
                 line = line.Substring(0, line.LastIndexOf(end));
@@ -43,7 +43,57 @@ public static class SubstringHandler {
         }
         
 
-        return "|Substring Handler Error|";
+        return line;//"|Substring Handler Error|";
+    }
+    
+    public static string[] Split (string line, string delimiter) {
+        return Split(line, new string[] {delimiter});
+    }
+    public static string[] Split (string line, string[] delimiters) {
+        
+        List<string> line_parameters = new List<string>();
+        int index = 0;
+
+        for (int i = 0; i < line.Length; i++) {
+
+            int delimiter_index = -1; 
+
+            for (int j = 0; j < delimiters.Length; j++) {
+                //Note: delimiters are assumed to be one character only... does this matter? would a delimiter of " (" or "; " be useful?
+                // for (int k = 0; k < delimiters[j].Length; k++) {
+                    if (line[i] == delimiters[j][0]) {
+                        delimiter_index = j;
+                    }
+                // }
+            }
+
+            if (delimiter_index != -1) {
+                //i += 1;
+                line_parameters.Add("");
+                index++;
+            }
+            else {
+                line_parameters[index] += line[i];
+            }
+        }
+
+        return line_parameters.ToArray();
+    }
+
+    public static int IndexInScopeOf (string line, string end) {
+
+        int count = 0;
+        int parenthesis_count = 1;
+
+        while (count < line.Length && parenthesis_count > 0) {
+            count++;
+            if (line_in[start_index + count].ToString () == Operators.OPENING_PARENTHESIS) {
+                parenthesis_count++;
+            } else if (line_in[start_index + count].ToString () == Operators.CLOSING_PARENTHESIS) {
+                parenthesis_count--;
+            }
+        }
+        return count;
     }
 
 }
