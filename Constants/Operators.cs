99182d2fe79209b27d390dfc99c9c6054e558642
Brian Hungerman <brianhungerman@gmail.com>
2019-05-29 20:01:25 -0700

    Improving encapsulation, readability of operators

--- Constants/Operators.cs
+++ Constants/Operators.cs
@@ -1,29 +1,31 @@
-﻿public static class Operators {
+public static class Operators {
+    /* 
+        See https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/
+        for an explicit breakdown of this heirarchy.
+    */
+    public static class Arithmetic {
+        public const string MULTIPLICATION = "*", DIVISION = "/", REMAINDER = "%", ADDITION = "+", SUBTRACTION = "-";   
+        public static class Unary {
+            public const string INCREMENT = "++", DECREMEMT = "--";
+        }
+        public static class Compound {
+           public const string MULTIPLICATION = "*=", DIVISION = "/=", REMAINDER = "%=", ADDITION = "+=", SUBTRACTION = "-=";
+        }
+    }
+    public static class Boolean {
+        public static const NEGATION = "!", AND = "&&", OR = "||", EQUALITY = "==", INEQUALITY = "!=";
+    }
+    public static class Comparison {
+        public static const LESS_THAN = "<", GREATER_THAN = ">", LESS_THAN_OR_EQUAL = "<=", GREATER_THAN_OR_EQUAL = ">=";
+    }
+    public static class Member {
+        public static const ACCESS = ".", INDEXER_OPENING = "[", INDEXER_CLOSING = "]", INVOCATION = "()";
+    }
     public const string EMPTY = "",
         SPACE = " ",
         COMMA = ",",
         DOT = ".",
         EQUALS = "=",
-        INCREMENT = "++",
-        DECREMENT = "--",
-        ADDITION = "+=",
-        SUBTRACTION = "-=",
-        MULTIPLICATION = "*=",
-        DIVISION = "/=",
-        MODULO = "%=",
-        EQUAL_TO = "==",
-        NOT_EQUAL = "!=",
-        GREATER_THAN = ">",
-        GREATER_THAN_EQUAL = ">=",
-        LESS_THAN = "<",
-        LESS_THAN_EQUAL = "<=",
-        AND = "&&",
-        OR = "||",
-        MODULUS = "%",
-        TIMES = "*",
-        DIVIDE = "/",
-        ADD = "+",
-        SUBTRACT = "-",
         OPENING_BRACKET = "{",
         CLOSING_BRACKET = "}",
         OPENING_PARENTHESIS = "(",
@@ -31,9 +33,6 @@
         END_LINE = ";",
         TAB = "\t",
         NEW_LINE = "\n";
-
-    //public const string[] ALL_OPERATORS = { EQUALS, INCREMENT, DECREMENT, ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION, MODULO, EQUAL_TO, NOT_EQUAL, GREATER_THAN, GREATER_THAN_EQUAL, LESS_THAN, LESS_THAN_EQUAL, AND, OR, MODULUS, TIMES, DIVIDE, ADD, SUBTRACT };
-
     public const char END_LINE_CHAR = ';';
 
 
