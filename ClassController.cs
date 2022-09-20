e82f4988def8bb97900638620646bc75433673d2
Brian Hungerman <brianhungerman@gmail.com>
2022-09-19 23:17:51 -0700

    Update ClassController.cs

--- ClassController.cs
+++ ClassController.cs
@@ -4,41 +4,53 @@ using UnityEngine;
 
 public class ClassController
 {
+    string name = "";
     List<Field> fields; // primitive data types, objects
     List<Method> methods; // constructor (initializes fields), getter/setter (modifies fields), main (Processor's start method)
 
     public ClassController(string input) {
+        fields = new List<Field>();
+        methods = new List<Method>();
+        
         switch (input) {
             case "◎": //Booster
+                name = "Booster";
                 fields.Add(new Field("throttle", "double", "0"));
-                methods.Add(new Method("Boost ()", "Throttle control", "throttle = 100;"));
-                methods.Add(new Method("Launch ()", "Torpedo control", "new Torpedo();"));
-                methods.Add(new Method("Ping ()", "Called periodically", "throttle--;"));
+                methods.Add(new Method("Boost ()", "Throttle_control", "void", "throttle = 100;"));
+                methods.Add(new Method("Launch ()", "Torpedo_control", "Torpedo", "return new Torpedo( GetLoadedBarrel() );"));
+                methods.Add(new Method("Ping ()", "Called_periodically", "void", "throttle--;"));
                 break;
-            case "◍": //Thruster
+            case "◉": //Thruster
+                name = "Thruster";
                 fields.Add(new Field("throttle", "double", "0"));
-                methods.Add(new Method("MaxThrottle ()", "Throttle control (max)", "throttle = 100;"));
-                methods.Add(new Method("MinThrottle ()", "Throttle control (min)", "throttle = 0;"));
+                methods.Add(new Method("MaxThrottle ()", "Throttle_control_(max)", "void", "throttle = 100;"));
+                methods.Add(new Method("MinThrottle ()", "Throttle_control_(min)", "void", "throttle = 0;"));
                 break;
             case "◍": //Cannon
+                name = "Cannon";
                 fields.Add(new Field("barrels", "double[]", "{}"));
-                methods.Add(new Method("Fire ()", "Use weapon control ", "new Shell();"));
+                methods.Add(new Method("Fire ()", "Use_weapon_control ", "Shell", "return new Shell( GetLoadedBarrel() );"));
+                break;
             case "▥": //Bulkhead
+                name = "Bulkhead";
                 fields.Add(new Field("heap", "Heap", "new Heap()"));
-                methods.Add(new Method("New (string name)", "Memory Allocation", "heap.Add(name)"));
-                methods.Add(new Method("Delete (string name)", "Memory Deallocation", "heap.Remove(name)"));
+                methods.Add(new Method("New (string name)", "Memory_Allocation", "void", "heap.Add( name )"));
+                methods.Add(new Method("Delete (string name)", "Memory_Deallocation", "void", "heap.Remove( name )"));
                 break;
             case "▩": //Processor
+                name = "Processor";
                 fields.Add(new Field("stack", "Stack", "new Stack()"));
-                methods.Add(new Method("Main (string[] args)", "Main Method (entry point)", "$"));
+                methods.Add(new Method("Main (string[] args)", "Main_Method_(entry_point)", "void", "$"));
                 break;
             case "▣": //Gimbal
-                methods.Add(new Method("RotateCW ()", "Rotation control (cw)", "r += 15;"));
-                methods.Add(new Method("RotateCCW ()", "Rotation control (ccw)", "r -= 15;"));
-                methods.Add(new Method("Rotate (double delta)", "Rotation control (ccw)", "r += delta;"));
+                name = "Gimbal";
+                methods.Add(new Method("RotateCW ()", "Rotation_control_(cw)", "void", "r += 15;"));
+                methods.Add(new Method("RotateCCW ()", "Rotation_control_(ccw)", "void", "r -= 15;"));
+                methods.Add(new Method("Rotate (double delta)", "Rotation_control_(ccw)", "void", "r += delta;"));
                 break;
             case "◌": //Sensor
-                methods.Add(new Method("Scan ()", "Measure distance", "new Ray().Length();"));
+                name = "Sensor";
+                methods.Add(new Method("Scan ()", "Measure_distance", "double", "new Ray().Length();"));
                 break;
             case "▦": //Printer
                 break;
@@ -51,9 +63,15 @@ public class ClassController
     }
 
     public override string ToString() {
-        return fields.ToArray() + methods.ToArray(); 
+        string output = "class " + name + " : Component {\n";
+        foreach (var f in fields.ToArray()) {
+            output += f.ToString() + "\n";
+        }
+        foreach (var m in methods.ToArray()) {
+            output += m.ToString() + "\n";
+        }
+        return output + "}";
     }
-
 }
 
 
@@ -65,13 +83,20 @@ public class Field
         this.type = type;
         this.value = value;
     }
+    public override string ToString () {
+        return type + " " + name + " = " + value + ";";
+    }
 }
 public class Method 
 {
-    public string comment, name, code;
-    public Method(string name, string comment, string code) {
+    public string comment, name, return_type, code;
+    public Method(string name, string comment, string return_type, string code) {
         this.comment = comment;
         this.name = name;
+        this.return_type = return_type;
         this.code = code;
+    }
+    public override string ToString () {
+        return "/*" + comment + "*/\n" + return_type + " " + name + " { " + code + " }";
     }
 }
