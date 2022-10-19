4c64844530e2e3750174791585e6bd82cc3cc849
Brian Hungerman <bitnaughts@gmail.com>
2022-10-18 20:50:04 -0700

    Update InterpreterV3.cs

--- InterpreterV3.cs
+++ InterpreterV3.cs
@@ -15,6 +15,9 @@ public class InterpreterV3
     private static int int_result;
     private static float float_result;
     private static bool bool_result;
+
+    // TODO- update clickable button names to be able to track where to make edits (Which class/method/line)
+
     /* InterpreterV3 takes a list of classes, one of which must contain a "Main" method */
     public InterpreterV3(List<ClassObj> classes) 
     {
@@ -205,13 +208,11 @@ public class InterpreterV3
         if (line == -1) 
         {
             line = s.start_index;
-            debug_output += "\nsetting index " + line;
         }
         if (line == s.end_index) 
         {
             if (s.is_loop) line = s.start_index;
             else if (scope.count() > 1) scope.pop();
-            debug_output += "\nending line " + line;
         }
         /* Grab the current line if empty */
         if (intermediate_line == "" || update_intermediate)  
@@ -225,8 +226,8 @@ public class InterpreterV3
             if (line >= s.method.lines.Count) line = 0;
             intermediate_line = s.method.lines[line].Trim().TrimEnd(';');
             update_intermediate = false;
-            debug_output += "\nLine " + line + ": \"" + intermediate_line + "\"";
         }
+        // debug_output += "\nLine " + line + ": \"" + intermediate_line + "\"";
         /* Determine operation */
         switch (intermediate_line[0]) 
         {
@@ -234,22 +235,26 @@ public class InterpreterV3
             case Operators.COMMENT_CHAR:
             case Operators.OPENING_BRACKET_CHAR:
             case Operators.CLOSING_BRACKET_CHAR:
-                debug_output += "Skipping";
                 return Step();
             default:
-                debug_output += "Operatable";
                 /* Split the intermediate line into its parts */
                 string[] line_parts = intermediate_line.Split (Operators.SPACE[0]);
                 string[] line_parameters = {};
                 var line_simplified = false;
                 switch (line_parts[0]) 
                 {
+                    case Keywords.Statement.Jump.CONTINUE:
+                        line = s.start_index;
+                        break;
+                    case Keywords.Statement.Jump.BREAK:
+                        line = s.end_index;
+                        scope.pop ();
+                        break;
                     /* Primitive Data Types (PDTs, Instantiated on Stack) */
                     case Keywords.Type.Value.BOOLEAN:
                     case Keywords.Type.Value.INTEGER:
                     case Keywords.Type.Value.DOUBLE:
                         line_parameters = new string[]{Simplify (intermediate_line.Split(Operators.EQUALS[0])[1], s, out line_simplified)};
-                        debug_output += "\nAFTER =" + intermediate_line.Split(Operators.EQUALS[0])[1] + "\nRESULT =" + line_parameters[0];
                         intermediate_line = line_parts[0] + " " + line_parts[1] + " = " + line_parameters[0];
                         if (line_simplified)
                         {
@@ -285,12 +290,72 @@ public class InterpreterV3
                             }
                             return Step();
                         }
-                        return true;            
+                        return true;
+                    case Keywords.Statement.Iteration.FOR:
+                        /* e.g. "for (int i = 5; i < 10; i++) {" */
+                        line_parameters = SubstringHandler.SplitFunction (intermediate_line, Operators.SPLIT_PARAMETERS);
+                        /* e.g. ["for", "int i = 5", "i < 10", "i++", "{"] */
+                        /* Has the for loop been executed in scope before? If so, "i" must've been declared */
+                        if (scope.isVariableInScope (line_parameters[1].Split (' ') [1])) {
+                            /* For loop has run in scope before, modify the variable */
+
+                            /* e.g. "i++" goes to "i = i + 1" goes to "i = 5 + 1" goes to "i = 6", simplified = true */
+                            line_parameters[3] = Parser.step (line_parameters[3], getVariableHandler (), out line_simplified);
+
+                            if (line_simplified) {
+                                scope.setVariableInScope (line_parameters[3]);
+
+                                first_section_finished = true;
+                                intermediate_line = line_parts[0] + "ll (" + line_parameters[1] + "; " + line_parameters[2] + "; " + line_parameters[3] + ")";
+
+                            }
+                            else {
+                                intermediate_line = line_parts[0] + " (" + line_parameters[1] + "; " + line_parameters[2] + "; " + line_parameters[3] + ")";
+                                break;
+                            }
+
+                        } else {
+                            /* For loop has not run in scope before, declare the variable */
+
+                            /* e.g. "int i = 5", simplified = true */
+                            line_parameters[1] = Parser.step (line_parameters[1], getVariableHandler (), out line_simplified);
+
+                            if (line_simplified) {
+                                Logger.Log (line_parameters[1]);
+                                scope.declareVariableInScope (line_parameters[1]);
+                                first_section_finished = true;
+                                intermediate_line = line_parts[0] + "ll (" + line_parameters[1] + "; " + line_parameters[2] + "; " + line_parameters[3] + ")";
+
+                            }
+                            else {
+                                intermediate_line = line_parts[0] + " (" + line_parameters[1] + "; " + line_parameters[2] + "; " + line_parameters[3] + ")";
+                                break;
+                            }; //don't check conditional in same instant
+                        }
+                        line_simplified = false;
+                        break;
+
+                    case Keywords.Statement.Iteration.FOR + "ll":
+                        line_parameters = SubstringHandler.SplitFunction(intermediate_line, Operators.SPLIT_PARAMETERS);
+
+                        //if (first_section_finished) {
+                        Logger.Log ("input:\n" + line_parameters[2]);
+                        line_parameters[2] = Parser.step (line_parameters[2], getVariableHandler (), out line_simplified);
+
+                        Logger.Log("output:\n" + line_parameters[2] + "\n" + script_saved[getPointer()]);
+
+                        if (line_simplified) {
+                            // scope.push (RangeObject.getScopeRange (script, getPointer ()), line_parts[0] != Keywords.Statement.Selection.IF);
+                            evaluateCondition (line_parameters[2], line_parts[0]);
+                            intermediate_line = script_saved[getPointer ()];
+                        } else {
+                            intermediate_line = line_parts[0] + " (" + line_parameters[1] + "; " + line_parameters[2] + "; " + line_parameters[3] + ") {";
+                        }
+
+                        break;            
                     default:
                         if (s.hasVariable (line_parts[0]) != -1) 
                         {
-                            // debug_output += line_parts[0] + "is variable!\n";
-                            
                             if (intermediate_line.Contains(Operators.EQUALS)) 
                             {
                                 line_parameters = new string[]{Simplify (intermediate_line.Split(Operators.EQUALS[0])[1], s, out line_simplified)};
@@ -304,98 +369,13 @@ public class InterpreterV3
                             }
                             /* CHECK IF LINE REFERS TO A VARIABLE, e.g. "i = 10;" */
                             // scope.setPrimitive(intermediate_line);
+                        } else {
+                            debug_output += "Not able to process line" + intermediate_line;
                         }
                         return false;
                 }
                 return true;
-            // case Keywords.Statement.Jump.CONTINUE:
-            //     index = s.start_index;
-            //     break;
-            // case Keywords.Statement.Jump.BREAK:
-            //     index = -1;
-            //     // scope.pop ();
-            //     break;
-            // case Keywords.Statement.Iteration.FOR:
-            //     /* For this case, use the example "for (int i = 5; i < 10; i++) {" */
-
-            //     /* e.g. ["for", "int i = 5", "i < 10", "i++", "{"] */
-            //     line_parameters = SubstringHandler.SplitFunction (intermediate_line, Operators.SPLIT_PARAMETERS);
-
-            //     /* Has the for loop been executed in scope before? If so, "i" must've been declared */
-            //     if (scope.isVariableInScope (line_parameters[1].Split (' ') [1])) {
-            //         /* For loop has run in scope before, modify the variable */
-
-            //         /* e.g. "i++" goes to "i = i + 1" goes to "i = 5 + 1" goes to "i = 6", simplified = true */
-            //         line_parameters[3] = Parser.step (line_parameters[3], getVariableHandler (), out line_simplified);
-
-            //         if (line_simplified) {
-            //             scope.setVariableInScope (line_parameters[3]);
-
-            //             first_section_finished = true;
-            //             script[getPointer()] = line_parts[0] + "ll (" + line_parameters[1] + "; " + line_parameters[2] + "; " + line_parameters[3] + ") {";
-
-            //         }
-            //         else {
-            //             script[getPointer ()] = line_parts[0] + " (" + line_parameters[1] + "; " + line_parameters[2] + "; " + line_parameters[3] + ") {";
-            //             break;
-            //         }
-
-            //     } else {
-            //         /* For loop has not run in scope before, declare the variable */
-
-            //         /* e.g. "int i = 5", simplified = true */
-            //         line_parameters[1] = Parser.step (line_parameters[1], getVariableHandler (), out line_simplified);
-
-            //         if (line_simplified) {
-            //             Logger.Log (line_parameters[1]);
-            //             scope.declareVariableInScope (line_parameters[1]);
-            //             first_section_finished = true;
-            //             script[getPointer()] = line_parts[0] + "ll (" + line_parameters[1] + "; " + line_parameters[2] + "; " + line_parameters[3] + ") {";
-
-            //         }
-            //         else {
-            //             script[getPointer ()] = line_parts[0] + " (" + line_parameters[1] + "; " + line_parameters[2] + "; " + line_parameters[3] + ") {";
-            //             break;
-            //         }; //don't check conditional in same instant
-            //     }
-            //     line_simplified = false;
-            //     break;
-
-            // case Keywords.Statement.Iteration.FOR + "ll":
-            //     line_parameters = SubstringHandler.SplitFunction(intermediate_line, Operators.SPLIT_PARAMETERS);
-
-            //     //if (first_section_finished) {
-            //     Logger.Log ("input:\n" + line_parameters[2]);
-            //     line_parameters[2] = Parser.step (line_parameters[2], getVariableHandler (), out line_simplified);
-
-            //     Logger.Log("output:\n" + line_parameters[2] + "\n" + script_saved[getPointer()]);
-
-            //     if (line_simplified) {
-            //         // scope.push (RangeObject.getScopeRange (script, getPointer ()), line_parts[0] != Keywords.Statement.Selection.IF);
-            //         evaluateCondition (line_parameters[2], line_parts[0]);
-            //         script[getPointer ()] = script_saved[getPointer ()];
-            //     } else {
-            //         script[getPointer ()] = line_parts[0] + " (" + line_parameters[1] + "; " + line_parameters[2] + "; " + line_parameters[3] + ") {";
-            //     }
-
-            //     break;
-
-            //     } else if (function_handler.isFunction (line_parts[0].Split ('(') [0])) {
-            //         /* CHECK IF LINE REFERS TO A FUNCTION, e.g. "print(x);" */
-            //         FunctionObject function = function_handler.getFunction (line_parts[0].Split ('(') [0]);
-            //         scope.push (RangeObject.returnTo (function.range, getPointer ()), false);
-
-            //     } else if (listener_handler.isFunction (line_parts[0].Split ('(') [0])) {
-
-            //         //TODO:: Not sure if this is accomplishing intended functionality
-
-            //         /* CHECK IF LINE REFERS TO A LISTENER, e.g. "Console.PrintLine("test");" */
-            //         // listener_handler.callListener (line,  obj);
-
-            //     } else {
-            //         debugger += "Not able to process line" + intermediate_line;
-            //     }
-            //     break;
+            //     
         }
         return false;
         // if (scope.isFinished ()) return true;
@@ -569,6 +549,7 @@ public class ScopeObj
     public override string ToString() 
     {
         string output = "Stack:\n";
+        output += stack.Peek().VariableString();
         foreach (var s in stack.ToArray ()) 
         {
             output += s.ToString () + "\n";
@@ -630,14 +611,18 @@ public class Scope
             variables[i].value = l[3];
         }
     }
-    public override string ToString() 
+    public string VariableString()
     {
-        string output = method.class_obj.name + "." + method.name + "<" + start_index + "," + end_index + ">";
+        string output = "";
         foreach (var v in variables) 
         {
-            output += "\n- " + v.ToString();
+            output += "- " + v.ToString() + "\n";
         }
         return output;
+    }
+    public override string ToString() 
+    {
+        return method.class_obj.name + "." + method.name + "<" + start_index + "," + end_index + ">";
     }
 }
 
@@ -650,6 +635,10 @@ public class HeapObj
     }
     public void add(string input) 
     {
+        if (heap.Contains(input)) 
+        {
+            heap.Remove(input);
+        }
         heap.Add(input);
     }
     public override string ToString() 
@@ -657,7 +646,7 @@ public class HeapObj
         string output = "Heap:\n";
         foreach (var h in heap.ToArray ()) 
         {
-            output += h + "\n";
+            output += h + ";\n";
         }
         return output;
     }
@@ -678,7 +667,7 @@ public class ClassObj
             // Iterate️ class
             case "ℹ":
                 name = "Iterate";
-                methods.Add(new Method(this, "Main", "_Entry_point_", "void", new List<Field>(){new Field("args", "String[]")}, new List<string>(){"//_New_line", "$", "int i = 2;", "int id = i * i * i;", "if (i + 1 < 5 + 2 * 4 + 10)", "{", "int j = 4;", "i = i + j + 1 + 2 + 3 + i + j;", "j = i + j + 1 + 2 + 3 + i + j;","int g = 1000 * 5 / 100;","Object obj = new Object();", "int k = 23 + 54 + 2342;", "}", "String test = \"Hello\";", "Object other = new Object();", "int abc = 123;", "double xyz = 987;", "bool primitive = false;", "bool otherbool = true;"}));
+                methods.Add(new Method(this, "Main", "_Entry_point_", "void", new List<Field>(){new Field("args", "String[]")}, new List<string>(){"//_New_line", "$", "String intro = \"Hello_World!\";", "bool primitive1 = false;", "bool primitive2 = true;"}));
                 // methods.Add(new Method("SumArray (int[] input)", "Return_sum_of_input_array", "int", "  int total = 0;\n  for (int i = 0; i < input.Length(); i++)\n  {\n    total += input[i];\n  }\n  return total;"));
                 break;
             // BitNaughts constant class
